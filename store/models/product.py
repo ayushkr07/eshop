1145ce937bbc77ba8cf44f9d260728968905de6a
Ayush Kumar <aks010598@gmail.com>
2020-07-22 11:24:57 +0530

    html display issue with image

--- store/models/product.py
+++ store/models/product.py
@@ -5,5 +5,10 @@ class Product(models.Model):
     name=models.CharField(max_length=50)
     price=models.IntegerField(default=0)
     cateogory=models.ForeignKey(Cateogory,on_delete=models.CASCADE,default=1)
-    description=models.CharField(max_length=200,default='')
+    description=models.CharField(max_length=200,default='',null=True,blank=True)
     image=models.ImageField(upload_to='upload/product/')
+
+
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
