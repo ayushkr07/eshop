8f25abd6dd37fdff1d1c856ab18592cd8f0ff020
Ayush Kumar <aks010598@gmail.com>
2020-07-24 18:36:50 +0530

    cart feature added

--- store/views.py
+++ store/views.py
@@ -10,12 +10,19 @@ class Index(View):
     def post(self,request):
         product = request.POST.get('product')
         cart = request.session.get('cart')
+        remove = request.POST.get('remove')
         if cart:
             quantity = cart.get(product)
-            if quantity:
-                cart[product] = quantity + 1
+            if remove:
+                if quantity <= 1:
+                    cart.pop(product)
+                else:
+                    cart[product] =quantity - 1
             else:
-                cart[product] = 1
+                if quantity:
+                    cart[product] = quantity + 1
+                else:
+                    cart[product] = 1
         else:
             cart ={}
             cart[product] = 1
@@ -24,6 +31,9 @@ class Index(View):
         return redirect('index')
 
     def get(self,request):
+        cart = request.session.get('cart')
+        if not cart:
+            request.session['cart']={}
         cateogories = Cateogory.get_all_cateogories()
         cateogoryID = request.GET.get('cateogory')
         if cateogoryID:
